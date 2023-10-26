e9bfa30c8879a363b82eb3871d53cf400796021b
Jorrit Strikwerda <jippiejorrit@gmail.com>
2023-10-26 13:56:05 +0200

    Add admin title

--- app/apps/feedback/admin.py
+++ app/apps/feedback/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.admin import AdminSite
 
 from .models import Feedback
 
@@ -7,6 +8,11 @@ class FeedbackAdmin(admin.ModelAdmin):
     list_display = ("meldr_nummer", "feedback_type", "aanmaakdatum")
     list_filter = ("feedback_type", "aanmaakdatum")
     search_fields = ("meldr_nummer",)
+
+
+AdminSite.site_title = "Feedback Admin"
+AdminSite.site_header = "Feedback Admin"
+AdminSite.index_title = "Feedback Admin"
 
 
 admin.site.register(Feedback, FeedbackAdmin)
