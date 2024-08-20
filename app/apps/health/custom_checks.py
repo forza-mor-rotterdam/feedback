316b70fe093bc63acf65ae2462961d93b8599c24
Jorrit Strikwerda <j.strikwerda@alteza.nl>
2024-08-20 15:12:42 +0200

    Fix health, add healtz,, update pre-commit.

--- app/apps/health/custom_checks.py
+++
@@ -1,8 +0,0 @@
-from health_check.backends import BaseHealthCheckBackend
-
-
-class MeldingenTokenCheck(BaseHealthCheckBackend):
-    critical_service = False
-
-    def identifier(self):
-        return self.__class__.__name__
